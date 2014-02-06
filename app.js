92838df4da2e39d36edc454a70fb1eec28bead94
Ali Al Dallal <ali@alicoding.com>
2014-02-06 15:18:23 -0500

    Final

---
+++ app.js
@@ -0,0 +1,31 @@
+var express = require('express'),
+    i18n = require('webmaker-i18n'),
+    nunjucks = require('nunjucks'),
+    path = require('path');
+
+var app = express(),
+    nunjucksEnv = new nunjucks.Environment( new nunjucks.FileSystemLoader( path.join( __dirname, 'views' )), {
+      autoescape: false
+    });
+
+nunjucksEnv.express( app );
+
+app.use( express.logger());
+
+var supportedLanguages = ['en-US', 'en-CA'];
+app.use( i18n.middleware({
+  supported_languages: supportedLanguages,
+  default_lang: 'en-US',
+  translation_directory: path.join( __dirname, 'locale' ),
+  mappings: {
+    'en': 'en-CA'
+  }
+}));
+
+app.get( "/", function( req, res ) {
+  res.render( "activity.html" );
+});
+
+app.listen(8000, function() {
+  console.log("Server listening - http://localhost:8000");
+});
